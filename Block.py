3c8eca538c371369421c75bd8637772b714c9329
Yuka Langbuana <langbuana.yuka@hotmail.com>
2018-03-13 13:46:45 -0700

    Add files via upload
    Adding Block class and blockchain.py to repository

---
+++ Block.py
@@ -0,0 +1,29 @@
+import hashlib as hasher
+
+class Block:
+
+    #This is our initiator function
+    #   We can add any data we want to the block
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index          = index
+        self.timestamp      = timestamp
+        self.data           = data
+        self.previous_hash  = previous_hash           #This is where our immutability came from
+        
+        self.hash           = self.hash_block()       #To ensure the integrity of our blockchain,
+                                                      #each block shall have a self-identifying hash.
+                                                      #Think of it as some sort of ID number.
+
+    #This function create our block's "ID Number"
+    #   made out of the block's index, timestamp, data, and previous block's hash
+    def hash_block(self):
+        sha = hasher.sha256()               
+        sha.update(str(self.index).encode('utf-8')      +
+                   str(self.timestamp) +
+                   str(self.data)      +
+                   str(self.previous_hash))
+        return sha.hexdigest()
+
+
+#This is our block class
+    
---
+++ blockchain.py
@@ -0,0 +1,33 @@
+from Block import *
+import datetime as date
+
+def genesis_block():
+    return Block(0, date.datetime.now(), "I am the first. Pullman, Washington", "0")
+
+# Function to generate new block
+def next_block(last_block):
+    this_index = last_block.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = "Gentry Coin block: " + str(this_index)
+    this_hash = last_block.hash
+
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+#=======================FOR TESTING PURPOSE==========================================
+
+# Create the blockchain and add the genesis block
+blockchain = [genesis_block()]
+previous_block = blockchain[0]
+
+# How many blocks should we add to the chain after the genesis block
+# We shall modify the structure of this code later to automate the process.
+num_of_blocks_to_add = 20
+
+# Add blocks to the chain
+for i in range(0, num_of_blocks_to_add):
+    block_to_add = next_block(previous_block)
+    blockchain.append(block_to_add)
+    previous_block = block_to_add
+    # Tell everyone about it!
+    print "Gentry Coin block", i, "has been added to the blockchain.".format(block_to_add.index)
+    print "Hash: {}\n".format(block_to_add.hash)
